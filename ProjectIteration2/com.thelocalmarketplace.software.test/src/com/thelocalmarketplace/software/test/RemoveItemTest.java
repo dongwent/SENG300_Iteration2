c02abc494a4a7f929c8499654a60e012f0e49514
Dongwen <tdongwen@gmail.com>
2023-11-20 13:57:08 -0700

    Create RemoveItemTest.java

---
+++ ProjectIteration2/com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/RemoveItemTest.java
@@ -0,0 +1,14 @@
+package com.thelocalmarketplace.software.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RemoveItemTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
