b396850cf485c8d85a35c1f4db22371dcd562ae7
aribanoman <aribanoman@localhost>
2023-11-20 18:52:20 -0700

    bronze test class

--- ProjectIteration2/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithBanknoteBronzeTest.java
+++ ProjectIteration2/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithBanknoteBronzeTest.java
@@ -1,5 +1,35 @@
 package com.thelocalmarketplace.software;
 
-public class PayWithBanknoteBronzeTest {
+import com.tdc.banknote.*;
+import java.math.BigDecimal;
+import java.util.Currency;
 
+public class PayWithBanknoteBronzeTest {
+    public static void main(String[] args) {
+        Currency currency = Currency.getInstance("USD");
+        BigDecimal initialAmountDue = BigDecimal.valueOf(100);
+
+        PayWithBanknoteBronze payWithBanknoteBronze = new PayWithBanknoteBronze(null, null, null, null, initialAmountDue, currency);
+
+        // Test with valid banknote
+        try {
+            Banknote validBanknote = new Banknote(currency, BigDecimal.valueOf(50));
+            payWithBanknoteBronze.payWithBanknote(validBanknote);
+            System.out.println("Test with valid banknote passed.");
+        } catch (Exception e) {
+            System.out.println("Test with valid banknote failed: " + e.getMessage());
+        }
+
+        // Test with invalid currency banknote
+        try {
+            Banknote invalidCurrencyBanknote = new Banknote(Currency.getInstance("EUR"), BigDecimal.valueOf(50));
+            payWithBanknoteBronze.payWithBanknote(invalidCurrencyBanknote);
+            System.out.println("Test with invalid currency banknote should have failed but passed.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Test with invalid currency banknote passed.");
+        } catch (Exception e) {
+            System.out.println("Test with invalid currency banknote failed with unexpected exception: " + e.getMessage());
+        }
+
+    }
 }
